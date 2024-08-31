3df45a8e416503fdc09e197acd949c8bf8fe4505
Connor Dailey <connorldailey@gmail.com>
2024-08-31 17:16:06 -0400

    Added plant cards and css

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get a reference to the button elements
+const monsteraButton = document.querySelector('#monstera-btn');
+
+const showPlantDetails = function() {
+    
+}
+
+// Add event listeners to buttons
+monsteraButton.addEventListener('click', showPlantDetails);
